43535276b24e035085ba9fa0ace5a2cfbed63a9c
Rustam <SpaceLudens1@gmail.com>
2024-02-06 17:04:56 +0300

    refactoring

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,10 +5,6 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Parameters;
 import picocli.CommandLine.Option;
 
-import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Scanner;
 import java.util.concurrent.Callable;
 
 import static hexlet.code.Differ.generate;
@@ -18,35 +14,25 @@ import static hexlet.code.Differ.generate;
         mixinStandardHelpOptions = true,
         description = "Compares two configuration files and shows a difference.")
 
-class App implements Callable {
-    static Scanner scanner = new Scanner(System.in);
-    static String nextLine = scanner.nextLine();
-    Path filePath1 = Paths.get("src/main/java/resources/file1.json");
-    Path absolutePath1 = filePath1.toAbsolutePath();
-    String absolutePathString1 = absolutePath1.toString();
-    Path filePath2 = Paths.get("src/main/java/resources/file2.json");
-    Path absolutePath2 = filePath2.toAbsolutePath();
-    String absolutePathString2 = absolutePath2.toString();
-
-    @Parameters(index = "0", description = "path to first file", paramLabel = "filepath1")
-    private File file1;
-    @Parameters(index = "0", description = "path to second file", paramLabel = "filepath2")
-    private File file2;
+class App implements Callable<Integer> {
+    @Parameters(index = "0",
+            description = "path to first file",
+            paramLabel = "filepath1")
+    private String filePath1;
+    @Parameters(index = "0",
+            description = "path to second file",
+            paramLabel = "filepath2")
+    private String filePath2;
     @Option(names = {"-f", "--format"},
             description = "output format [default: stylish]",
             paramLabel = "format")
-
+    String format;
     @Override
-    public Object call() throws Exception {
-        if (nextLine.equals("file1.json file2.json")) {
-            file1 = new File(absolutePathString1);
-            file2 = new File(absolutePathString2);
-            System.out.println(generate(file1, file2));
-        }
-
+    public Integer call() throws Exception {
+        System.out.println(generate(filePath1, filePath2));
         return null;
     }
     public static void main(String[] args) {
-        new CommandLine(new App()).execute(nextLine);
+        System.exit(new CommandLine(new App()).execute(args));
     }
 }
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -3,16 +3,31 @@ package hexlet.code;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.File;
-import java.io.IOException;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 
 public class Differ {
-    public static String generate (File file1, File file2) throws IOException {
+    public static String generate (String filePath1, String filePath2) throws Exception {
+        Path path1 = Paths.get(filePath1).toAbsolutePath().normalize();
+        Path path2 = Paths.get(filePath2).toAbsolutePath().normalize();
+
+        if (!Files.exists(path1)) {
+            throw new Exception("File '" + path1 + "does not exist");
+        }
+        if (!Files.exists(path2)) {
+            throw new Exception("File '" + path2 + "does not exist");
+        }
+
+        String content1 = Files.readString(path1);
+        String content2 = Files.readString(path2);
         ObjectMapper objectMapper = new ObjectMapper();
-        Map<String, Object> fileMap1 = objectMapper.readValue(file1, new TypeReference<>() {});
-        Map<String, Object> fileMap2 = objectMapper.readValue(file2, new TypeReference<>() {});
+
+        Map<String, Object> fileMap1 = objectMapper.readValue(content1, new TypeReference<>() {});
+        Map<String, Object> fileMap2 = objectMapper.readValue(content2, new TypeReference<>() {});
 
         Map<String, Object> sortedMap = new TreeMap<>(fileMap1);
         StringBuilder result = new StringBuilder();
