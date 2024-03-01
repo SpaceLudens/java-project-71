6daa1eacaed825ad00d7ace419754321762057e1
SpaceLudens <SpaceLuden1@gmail.com>
2024-03-01 12:01:11 +0300

    refactoring

--- app/src/test/java/io/hexlet/ApplicationTest.java
+++ app/src/test/java/io/hexlet/ApplicationTest.java
@@ -82,6 +82,38 @@ public class ApplicationTest {
     }
 
     @Test
+    public void testGenerateWithStylishFormatter() throws Exception {
+        String expected = """
+                {
+                    chars1: [a, b, c]
+                  - chars2: [d, e, f]
+                  + chars2: false
+                  - checked: false
+                  + checked: true
+                  - default: null
+                  + default: [value1, value2]
+                  - id: 45
+                  + id: null
+                  - key1: value1
+                  + key2: value2
+                    numbers1: [1, 2, 3, 4]
+                  - numbers2: [2, 3, 4, 5]
+                  + numbers2: [22, 33, 44, 55]
+                  - numbers3: [3, 4, 5]
+                  + numbers4: [4, 5, 6]
+                  + obj1: {nestedKey=value, isNested=true}
+                  - setting1: Some value
+                  + setting1: Another value
+                  - setting2: 200
+                  + setting2: 300
+                  - setting3: true
+                  + setting3: none
+                }""";
+        String pathJson2 = "src/test/resources/file2.json";
+        String actual = generate(pathJson1, pathJson2);
+        assertEquals(expected, actual);
+    }
+    @Test
     public void testGenerateWithPlainFormatter() throws Exception {
         String expected = """
                 Property 'chars2' was updated. From [complex value] to false
