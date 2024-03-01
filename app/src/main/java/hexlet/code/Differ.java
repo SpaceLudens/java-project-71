85dfa0ea3458c1de17516d33e7c21de23c483561
SpaceLudens <SpaceLuden1@gmail.com>
2024-03-01 12:12:24 +0300

    refactoring

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -13,43 +13,6 @@ import static hexlet.code.Formatter.formatter;
 import static hexlet.code.Parser.parser;
 
 public class Differ {
-    public static String generate(String filePath1, String filePath2) throws Exception {
-
-        Map<String, Object> map1 = new TreeMap<>(parser(filePath1));
-        Map<String, Object> map2 = new TreeMap<>(parser(filePath2));
-
-        Set<String> keys = new TreeSet<>();
-        keys.addAll(map1.keySet());
-        keys.addAll(map2.keySet());
-
-        List<Map<String, Object>> result = new ArrayList<>();
-
-        for (String key : keys) {
-            Map<String, Object> mergeMap = new LinkedHashMap<>();
-            if (!map1.containsKey(key)) {
-                mergeMap.put("key", key);
-                mergeMap.put("oldValue", map2.get(key));
-                mergeMap.put("changes", "added");
-                result.add(mergeMap);
-            } else if (!map2.containsKey(key)) {
-                mergeMap.put("key", key);
-                mergeMap.put("oldValue", map1.get(key));
-                mergeMap.put("changes", "removed");
-                result.add(mergeMap);
-            } else if (Objects.equals(map1.get(key), map2.get(key))) {
-                mergeMap.put("key", key);
-                mergeMap.put("oldValue", map1.get(key));
-                result.add(mergeMap);
-            } else {
-                mergeMap.put("key", key);
-                mergeMap.put("oldValue", map1.get(key));
-                mergeMap.put("newValue", map2.get(key));
-                mergeMap.put("changes", "updated");
-                result.add(mergeMap);
-            }
-        }
-        return formatter(result, "stylish");
-    }
     public static String generate(String filePath1, String filePath2, String format) throws Exception {
 
         Map<String, Object> map1 = new TreeMap<>(parser(filePath1));
@@ -86,5 +49,9 @@ public class Differ {
             }
         }
         return formatter(result, format);
+    }
+
+    public static String generate(String filePath1, String filePath2) throws Exception {
+        return generate(filePath1,filePath2, "stylish");
     }
 }
